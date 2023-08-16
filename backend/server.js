68f9baf6017b6609f54ba99d0905cf03052c073c
indianDeveloper0827 <warjun31@gmail.com>
2023-08-16 17:40:37 +0530

    added schema model

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from "express";
-import userRoutes from './routes/userRoutes.js'
-import dotenv from 'dotenv'
-import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-    dotenv.config()
+dotenv.config();
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users',userRoutes)
-app.get('/',(req,res) => res.send("Yes server is ready"))
+app.use("/api/users", userRoutes);
+app.get("/", (req, res) => res.send("Yes server is ready"));
 
+app.use(notFound);
+app.use(errorHandler);
 
-app.use(notFound)
-app.use(errorHandler)
-
-app.listen(port,() =>  console.log(`app is listen on PORT ${port}`))
+app.listen(port, () => console.log(`app is listen on PORT ${port}`));

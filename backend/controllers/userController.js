d6fd69f0e36304f5722529c90593401aae73946d
indianDeveloper0827 <warjun31@gmail.com>
2023-08-17 18:46:03 +0530

    add login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,31 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+
+  const {email,password} = req.body;
+
+  const user =  User.findOne({email})
+
+  if(user && (await user.matchPassword(password))) {
+    generateToken(res,user._id)
+
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    })
+  }
+  else{
+    res.status(401);
+    throw new Error('Invalid email or password')
+  }
+
 });
 
 // @desc Register a new user
@@ -31,6 +50,8 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if(user) {
+    generateToken(res,user._id)
+
     res.status(201).json({
         _id: user._id,
         name: user.name,

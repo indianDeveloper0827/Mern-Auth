b2dde29d350269def68476770189ad30324649cb
indianDeveloper0827 <warjun31@gmail.com>
2023-08-18 18:47:15 +0530

    add header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,32 +1,19 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap';
+import {Navbar,Nav,Container} from 'react-bootstrap'
+import {FaSignInAlt} from 'react-icons/fa'
 
 const Header = () => {
   return (
     <header>
     <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-      <Container>
-        arjun
-        {/* <LinkContainer to='/'>
-          <Navbar.Brand>MERN Auth</Navbar.Brand>
-        </LinkContainer> */}
-        {/* <Navbar.Toggle aria-controls='basic-navbar-nav' /> */}
-        {/* <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto'>
-                <LinkContainer to='/login'>
-                  <Nav.Link>
-                    <FaSignInAlt /> Sign In
-                  </Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/register'>
-                  <Nav.Link>
-                    <FaSignOutAlt /> Sign Up
-                  </Nav.Link>
-                </LinkContainer>
-          </Nav>
-        </Navbar.Collapse> */}
-      </Container>
+    <Container>
+      <Navbar.Brand href='/'>Mern App</Navbar.Brand>
+      <Navbar.Toggle aria-aria-controls='basic-navbar-nav'/>
+      <Navbar.Collapse>
+        <Nav className='ms-auto'>
+        <Nav.Link><FaSignInAlt/>Sign In</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
     </Navbar>
   </header>
   )
